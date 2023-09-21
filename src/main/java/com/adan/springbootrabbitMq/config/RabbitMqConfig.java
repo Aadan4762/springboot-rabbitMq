a0bd7f12a39603b77e7c599f98ef957869a3c143
adesh <aadan4762@gmail.com>
2023-09-21 12:05:39 +0300

    producer created

--- src/main/java/com/adan/springbootrabbitMq/config/RabbitMqConfig.java
+++ src/main/java/com/adan/springbootrabbitMq/config/RabbitMqConfig.java
@@ -1,5 +1,7 @@
 package com.adan.springbootrabbitMq.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,15 +15,29 @@ public class RabbitMqConfig {
     private String exchange;
     @Value("${rabbitmq.queue.name}")
     private String queue;
+    @Value("${rabbitmq.routingKey.name}")
+    private String routingKey;
 
-
+//create Queue
     @Bean
     public Queue queue(){
         return new Queue(queue);
     }
-
+//create exchange
     @Bean
     public TopicExchange exchange(){
         return new TopicExchange(exchange);
     }
+
+    //bind exchange to queue
+    @Bean
+    public Binding binding(){
+        return BindingBuilder
+                .bind(queue())
+                .to(exchange())
+                .with(routingKey);
+    }
+    //Connection Factory
+    //RabbitTemplate
+    //RabbitAdmin
 }
